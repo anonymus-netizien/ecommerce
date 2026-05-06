b5154f9d531ccb26b34d495c6a11c16d1884d439
anonymus-netizien <kollururamchandarrao@gmail.com>
2026-05-06 11:19:19 +0530

    chore: remove ignored files from repository

--- src/main/java/com/stschool/ecommerce/controller/CustomerController.java
+++ src/main/java/com/stschool/ecommerce/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.stschool.ecommerce.controller;
 
-import com.stschool.ecommerce.exception.CustomerExistsException;
 import com.stschool.ecommerce.exception.CustomerNotFoundException;
 import com.stschool.ecommerce.model.Customer;
 import com.stschool.ecommerce.service.CustomerService;
@@ -16,12 +15,6 @@ public class CustomerController {
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Customer createCustomer(@RequestBody Customer customer) throws CustomerExistsException {
-        return customerService.save(customer);
     }
 
     @GetMapping("/")
